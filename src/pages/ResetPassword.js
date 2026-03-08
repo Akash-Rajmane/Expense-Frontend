5cb1af726d45203ec933990e8da8e98f54832b39
Akash <akashrajmane23@gmail.com>
2026-03-08 17:59:12 +0530

    fix: html parser package change

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -1,84 +1,89 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import DOMPurify from "dompurify";
-import ReactHtmlParser from "react-html-parser";
+import parse from "html-react-parser";
 
 const ResetPassword = () => {
-    const { id } = useParams();
-    const [htmlContent, setHtmlContent] = useState('');
-    const navigate = useNavigate();
+  const { id } = useParams();
+  const [htmlContent, setHtmlContent] = useState("");
+  const navigate = useNavigate();
 
-    const fetchPage = async (id) => {
-        try {
-            const response = await fetch(`${process.env.REACT_APP_BE_HOST}/password/reset-password/${id}`);
-            if (response.status === 404) {
-                alert("Reset Password Request not found!");
-                navigate("/forgot-password");
-                return;
-            }
-            if (response.status === 401) {
-                alert("Reset Password Request is already expired!");
-                navigate("/forgot-password");
-                return;
-            }
+  const fetchPage = async (id) => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_BE_HOST}/password/reset-password/${id}`,
+      );
+      if (response.status === 404) {
+        alert("Reset Password Request not found!");
+        navigate("/forgot-password");
+        return;
+      }
+      if (response.status === 401) {
+        alert("Reset Password Request is already expired!");
+        navigate("/forgot-password");
+        return;
+      }
 
-            const data = await response.text();
-            setHtmlContent(DOMPurify.sanitize(data));
-        } catch (error) {
-            console.error('Error fetching ResetPassword page:', error);
+      const data = await response.text();
+      setHtmlContent(DOMPurify.sanitize(data));
+    } catch (error) {
+      console.error("Error fetching ResetPassword page:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPage(id);
+  }, [id]);
+
+  useEffect(() => {
+    const handlePasswordReset = async () => {
+      const newPassword = document.getElementById("password").value;
+      if (!newPassword) {
+        alert("Please enter a new password.");
+        return;
+      }
+
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_BE_HOST}/password/update-password/${id}`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ password: newPassword }),
+          },
+        );
+
+        const responseData = await response.json();
+
+        if (response.ok) {
+          alert(responseData.message);
+          navigate("/auth");
+        } else {
+          alert(responseData.message);
         }
+      } catch (error) {
+        console.error("Error:", error);
+        alert(
+          "An error occurred while resetting the password. Please try again later.",
+        );
+      }
     };
 
-    useEffect(() => {
-        fetchPage(id);
-    }, [id]);
+    const btn = document.getElementById("btn");
+    if (btn) {
+      btn.addEventListener("click", handlePasswordReset);
+    }
 
-    useEffect(() => {
-        const handlePasswordReset = async () => {
-            const newPassword = document.getElementById('password').value;
-            if (!newPassword) {
-                alert('Please enter a new password.');
-                return;
-            }
+    return () => {
+      if (btn) {
+        btn.removeEventListener("click", handlePasswordReset);
+      }
+    };
+  }, [htmlContent, id, navigate]);
 
-            try {
-                const response = await fetch(`${process.env.REACT_APP_BE_HOST}/password/update-password/${id}`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ password: newPassword }),
-                });
-
-                const responseData = await response.json();
-
-                if (response.ok) {
-                    alert(responseData.message);
-                    navigate("/auth");
-                } else {
-                    alert(responseData.message);
-                }
-            } catch (error) {
-                console.error('Error:', error);
-                alert('An error occurred while resetting the password. Please try again later.');
-            }
-        };
-
-        const btn = document.getElementById('btn');
-        if (btn) {
-            btn.addEventListener('click', handlePasswordReset);
-        }
-
-        return () => {
-            if (btn) {
-                btn.removeEventListener('click', handlePasswordReset);
-            }
-        };
-    }, [htmlContent, id, navigate]);
-
-    return (
-        <div className='auth'>{ReactHtmlParser(htmlContent)}</div>
-    );
+  return <div className="auth">{parse(htmlContent)}</div>;
 };
 
 export default ResetPassword;
